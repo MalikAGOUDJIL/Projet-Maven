72d2b6d9c621f4ddd5c03a7ff8cc880fc0935b55
Malik AGOUDJIL <malik.agoudjil@gmail.com>
2023-02-22 14:24:47 +0100

    premier commit

---
+++ Exemple.java
@@ -0,0 +1,21 @@
+package com.example ;
+import javax.swing.JOptionPane ;
+
+public class Exemple {
+     /**
+    * methode Main
+    *
+    */
+     /**
+     *comment
+     */
+     
+  public static void main (String[] args) {
+   //toDo 
+
+    system.out.println("Hello World") ;
+    try {
+    }catch(Exception e) {
+    }
+  }
+}
